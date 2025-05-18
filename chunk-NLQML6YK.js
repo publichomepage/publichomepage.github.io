b9da1bd174b3b8a7b34879197ea9742f10255462
RichDev <sandipsahoo2k2@gmail.com>
2025-05-18 13:06:50 -0400

    Auto-generated commit

---
+++ chunk-NLQML6YK.js
@@ -0,0 +1,1 @@
+import"./chunk-75BEUDPJ.js";import"./chunk-4ZZIO3ZI.js";
---
+++ chunk-O6E4JZHF.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e,g as f}from"./chunk-E4ERW3Q5.js";import"./chunk-4ZZIO3ZI.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
